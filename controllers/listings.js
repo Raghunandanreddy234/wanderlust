71f24bbbd2d3100146fef36331241b30ba0e5d17
Raghunandanreddy <raghupothareddy23@gmail.com>
2026-04-27 21:05:29 +0530

    changed files

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,4 +1,7 @@
 const Listing = require("../models/listing.js");
+const maptilerClient = require("@maptiler/client");
+
+maptilerClient.config.apiKey = process.env.MAPTILER_API_KEY;
 
 module.exports.index = async (req, res) => {
     const allListings = await Listing.find({});
@@ -27,26 +30,27 @@ module.exports.showListing =  async (req, res) => {
      res.render("listings/show", { listing, MAPTILER_API_KEY:process.env.MAPTILER_API_KEY});
 };
 
-module.exports.createListing = (async (req, res) => {
-    let coordinate = await geocodingClient.forwardGeocode({
-    query: 'New Delhi, India',
-     limit: 1,
-   })
-     .send();
+module.exports.createListing = async (req, res) => {
+    const response = await maptilerClient.geocoding.forward(
+        "New Delhi, India",
+        { limit: 1 }
+    );
 
-     console.log(coordinate);
-     res.send("Done!");
-     
+    console.log(response.body.features);
+  
 
-   let url = req.file.path;
-   let filename = req.file.filename;
+    let url = req.file.path;
+    let filename = req.file.filename;
+
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
-    newListing.image = {url, filename };
+    newListing.image = { url, filename };
+
     await newListing.save();
+
     req.flash("success", "New Listing Created!");
     res.redirect("/listings");
-});
+};
 
 module.exports.editListing = async (req, res) => {
     let { id } = req.params;
