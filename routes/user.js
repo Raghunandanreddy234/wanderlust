f0337b69cfdd95d8ff4f7797dc4afb9b37d154eb
Raghunandanreddy <raghupothareddy23@gmail.com>
2026-06-13 11:54:49 +0530

    re-updated render deployment

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require("passport");
 const User = require("../models/user.js");
-const wrapAsync = require("../utils/WrapAsync.js");
+const wrapAsync = require("../utils/wrapAsync.js");
 
 const { saveRedirectUrl } = require("../middleware.js");
 
